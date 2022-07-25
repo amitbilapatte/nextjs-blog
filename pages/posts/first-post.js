30c2a753b1e3c90df0ab0ee3437f01dd246cacea
Amit Bilapatte <agbilapatte78@gmail.com>
2022-07-25 16:06:08 +0530

    commit all for first deployment

---
+++ pages/posts/first-post.js
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import Image from 'next/image';
+import Head from 'next/head';
+import Script from 'next/script';
+import Layout from '../../components/layout';
+
+export default function FirstPost() {
+
+const YourComponent = () => (
+    <Image
+      src="/images/profile.png" // Route of the image file
+      height={144} // Desired size with correct aspect ratio
+      width={144} // Desired size with correct aspect ratio
+      alt="Your Name"
+    />
+  );
+
+  return (
+    <Layout>
+    <Head>
+        <title>First Post</title>
+        <script src="https://connect.facebook.net/en_US/sdk.js" />
+    </Head>
+    <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+    />
+    <h1>First Post</h1>
+    <h2>
+        <Link href="/">
+            <a>Back to home</a>
+        </Link>
+    </h2>
+    <YourComponent/>
+    </Layout>
+  )
+}
